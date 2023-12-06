57f1746643053a7134eefea12b80aa4b36fe00d5
Christian Fuchs <kontaktierbar@gmail.com>
2023-12-06 20:28:01 +0100

    add ngrx store-devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(StoreModule.forRoot()),
+    importProvidersFrom(
+      StoreModule.forRoot(),
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    ),
   ]
 };
