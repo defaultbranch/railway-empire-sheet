abe68e4c95b8ee4191dc635411faaefc64bcf95b
Christian Fuchs <kontaktierbar@gmail.com>
2023-12-07 18:45:31 +0100

    use routing to show one component at a time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 
-import { CiudadesTableComponent } from "./ciudades-table/ciudades-table.component";
 import { loadCiudades } from './ciudad.actions';
-import { GoodsTableComponent } from "./goods-table/goods-table.component";
 import { loadGoods } from './goods.actions';
-import { NegociosRuralesTableComponent } from "./negocios-rurales-table/negocios-rurales-table.component";
 import { loadNegociosRurales } from './negocio-rural.actions';
 
 @Component({
@@ -18,9 +15,7 @@ import { loadNegociosRurales } from './negocio-rural.actions';
     imports: [
         CommonModule,
         RouterOutlet,
-        CiudadesTableComponent,
-        GoodsTableComponent,
-        NegociosRuralesTableComponent
+        RouterLink,
     ]
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'ciudades', loadComponent: () => import('./ciudades-table/ciudades-table.component').then(c => c.CiudadesTableComponent) },
+  { path: 'goods', loadComponent: () => import('./goods-table/goods-table.component').then(c => c.GoodsTableComponent) },
+  { path: 'rurales', loadComponent: () => import('./negocios-rurales-table/negocios-rurales-table.component').then(c => c.NegociosRuralesTableComponent) },
+];
