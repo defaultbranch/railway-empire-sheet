820d58ef2d84325ac62bd543d1449a6b614b8dd2
Christian Fuchs <kontaktierbar@gmail.com>
2023-12-08 18:20:22 +0100

    add Ciudad industry setters

--- src/app/ciudad/ciudad.component.ts
+++ src/app/ciudad/ciudad.component.ts
@@ -7,7 +7,8 @@ import { Store } from '@ngrx/store';
 
 import { Ciudad } from '../ciudad';
 import { ciudad } from '../ciudad.state';
-import { updatePopulation } from '../ciudad.actions';
+import { updateBusiness, updatePopulation } from '../ciudad.actions';
+import { allIndustrieNames } from '../industrias.state';
 
 @Component({
   selector: 'app-ciudad',
@@ -22,6 +23,7 @@ import { updatePopulation } from '../ciudad.actions';
 export class CiudadComponent implements OnDestroy {
 
   ciudad$: Observable<Ciudad | undefined>;
+  industrias$: Observable<string[]>;
 
   private disposing$ = new Subject<void>();
 
@@ -34,6 +36,7 @@ export class CiudadComponent implements OnDestroy {
       switchMap(name => store.select(ciudad(name))),
       takeUntil(this.disposing$)
     );
+    this.industrias$ = store.select(allIndustrieNames);
   }
 
   ngOnDestroy(): void {
@@ -42,5 +45,12 @@ export class CiudadComponent implements OnDestroy {
 
   updatePopulation(p: { name: string, population: number }) {
     this.store.dispatch(updatePopulation({ name: p.name, population: p.population }));
+  }
+
+  updateBusiness(p: { name: string, index: number, business: string | undefined, size: number | undefined }) {
+    console.log(p);
+    if (p.index >= 0 && p.index <= 3 && !!p.business && !!p.size && p.size >= 1 && p.size <= 5) {
+      this.store.dispatch(updateBusiness({ name: p.name, index: p.index, business: p.business, size: p.size }));
+    }
   }
 }
