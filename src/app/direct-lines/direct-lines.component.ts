b2bcabeca281322ed9baa8d03f6a254f267b03e5
Christian Fuchs <kontaktierbar@gmail.com>
2023-12-09 19:45:34 +0100

    impl sortByNextRun

--- src/app/direct-lines/direct-lines.component.ts
+++ src/app/direct-lines/direct-lines.component.ts
@@ -64,6 +64,25 @@ export class DirectLinesComponent {
     this.linesSorted$ = this.lines$.pipe(map(it => it.sort((a, b) => a.miles - b.miles)));
   }
 
+  sortByNextRun() {
+    this.linesSorted$ = combineLatest([
+      this.lines$,
+      this.rurales$,
+      this.ciudades$,
+    ]).pipe(
+      map(([lines, rurales, ciudades]) => lines.map(line => {
+        const productionPerWeek = rurales.find(it => it.name === line.ruralProducer && it.product === line.good)?.perWeek ?? 0;
+        const demandPerWeek = (ciudades.find(it => it.name === line.destinationCity)?.perWeek ?? {})[line.good] ?? 0;
+        const effectiveRate = this.effectiveRate({ line, productionPerWeek, demandPerWeek});
+        return {
+          ...line,
+          nextRun: this.nextRun(line, effectiveRate)
+        }
+      })),
+      map(lines => lines.sort((a, b) => (a.nextRun?.getTime() ?? 0) - (b.nextRun?.getTime() ?? 0)))
+    );
+  }
+
   productionPerWeek$(line: DirectLine): Observable<number> {
     return this.rurales$.pipe(
       concatMap(it => from(it)),
@@ -80,27 +99,35 @@ export class DirectLinesComponent {
     )
   }
 
+  effectiveRate(p: { line: DirectLine, productionPerWeek: number, demandPerWeek: number }) {
+    return Math.min(
+      p.productionPerWeek * (p.line.productionFactor ?? 1.0),
+      p.demandPerWeek * (p.line.demandFactor ?? 1.0));
+  }
+
   effectiveRate$(line: DirectLine): Observable<number> {
     return combineLatest([this.productionPerWeek$(line), this.demandPerWeek$(line)]).pipe(
-      map(([prod, demand]) => Math.min(prod * (line.productionFactor ?? 1.0), demand * (line.demandFactor ?? 1.0)))
+      map(([prod, demand]) => this.effectiveRate({ line, productionPerWeek: prod, demandPerWeek: demand }))
     );
   }
 
   runNow(line: DirectLine) {
-    this.gameDate$.pipe(take(1)).subscribe(date => this.store.dispatch(runDirectLineNow({ line, date})));
+    this.gameDate$.pipe(take(1)).subscribe(date => this.store.dispatch(runDirectLineNow({ line, date })));
+  }
+
+  nextRun(line: DirectLine, effectiveRate: number) {
+    if (line.lastRun) {
+      const nextRun = new Date(line.lastRun);
+      nextRun.setDate(nextRun.getDate() + 56 / effectiveRate);
+      return nextRun;
+    } else {
+      return undefined;
+    }
   }
 
   nextRun$(line: DirectLine): Observable<Date | undefined> {
     return combineLatest([this.effectiveRate$(line)]).pipe(
-      map(([effectiveRate]) => {
-        if (line.lastRun) {
-          const nextRun = new Date(line.lastRun);
-          nextRun.setDate(nextRun.getDate() + 56/effectiveRate);
-          return nextRun;
-        } else {
-          return undefined;
-        }
-      })
+      map(([effectiveRate]) => this.nextRun(line, effectiveRate))
     )
   }
 }
