dae667fc4e7c227d257f984d92a2198f64d38664
Christian Fuchs <kontaktierbar@gmail.com>
2023-12-30 14:49:38 +0100

    naming and documentation

--- src/app/game-state/util.ts
+++ src/app/game-state/util.ts
@@ -2,14 +2,21 @@ import { Observable, concatMap, from, map, switchMap, take, toArray } from "rxjs
 
 // 'util.ts' is a collection of loose code snippets; this should be cleaned up eventually
 
-export function sortObservableStream<T, C>(
-  items$: Observable<T[]>,
-  lookup: (item: T) => Observable<C>,
-  compare: (a: C, b: C) => number,
-): Observable<T[]> {
+/**
+ *
+ * @param items$ observable array of items, unsorted
+ * @param toCriterion map one item to observable criteria value
+ * @param compare compare criteria value
+ * @returns observable array of items, sorted
+ */
+export function sortObservableStream<TValue, TCriterion>(
+  items$: Observable<TValue[]>,
+  toCriterion: (item: TValue) => Observable<TCriterion>,
+  compare: (a: TCriterion, b: TCriterion) => number,
+): Observable<TValue[]> {
   return items$.pipe(
     switchMap(items => from(items).pipe(
-      concatMap(item => lookup(item).pipe(
+      concatMap(item => toCriterion(item).pipe(
         map(criteria => ([criteria, item] as const)),
         take(1)
       )),
