9c1533232c1651462fad8e486237f2236982bc36
Christian Fuchs <kontaktierbar@gmail.com>
2024-02-16 11:45:32 +0100

    add more data to load-and-save

--- src/app/load-and-save/load-and-save.component.ts
+++ src/app/load-and-save/load-and-save.component.ts
@@ -7,6 +7,8 @@ import JSONEditor, { JSONEditorOptions } from 'jsoneditor';
 import { GoodsNgrxModule, allGoods, loadGoods } from '../game-config/ngrx/goods.ngrx';
 import { NegociosNgrxModule, loadNegocios, todosLosNegocios } from '../game-config/ngrx/negocios.ngrx';
 import { GameDateNgrxModule, gameDate, loadGameDate } from '../game-state/ngrx/game-date.ngrx';
+import { IndustriasNgrxModule, allIndustries, loadIndustrias } from '../game-config/ngrx/industrias.ngrx';
+import { DemandsNgrxModule, allDemands, loadDemands } from '../game-config/ngrx/demands.ngrx';
 
 @Component({
   selector: 'app-load-and-save',
@@ -16,6 +18,8 @@ import { GameDateNgrxModule, gameDate, loadGameDate } from '../game-state/ngrx/g
     GameDateNgrxModule,
     GoodsNgrxModule,
     NegociosNgrxModule,
+    IndustriasNgrxModule,
+    DemandsNgrxModule,
   ],
   templateUrl: './load-and-save.component.html',
   styleUrl: './load-and-save.component.scss'
@@ -33,6 +37,8 @@ export class LoadAndSaveComponent implements OnInit, AfterViewInit, OnDestroy {
     store.dispatch(loadGameDate());
     store.dispatch(loadGoods());
     store.dispatch(loadNegocios());
+    store.dispatch(loadIndustrias());
+    store.dispatch(loadDemands());
   }
 
   ngOnInit(): void {
@@ -41,15 +47,21 @@ export class LoadAndSaveComponent implements OnInit, AfterViewInit, OnDestroy {
       this.store.select(gameDate),
       this.store.select(allGoods),
       this.store.select(todosLosNegocios),
+      this.store.select(allIndustries),
+      this.store.select(allDemands),
     ]).pipe(
       map(([
         gameDate,
         goods,
         negocios,
+        industrias,
+        demands,
       ]) => ({
         gameDate: gameDate,
         goods: goods,
         negocios: negocios,
+        industrias: industrias,
+        demands: demands,
       })),
       takeUntil(this.disposing$)
     ).subscribe(it => this.data$.next(it));
