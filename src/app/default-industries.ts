961fe6b6fddd6ed7f5672113521fc6bde1419884
Christian Fuchs <kontaktierbar@gmail.com>
2026-09-22 21:49:47 +0200

    restrict contract for defaultIndustries

--- src/app/default-industries.ts
+++ src/app/default-industries.ts
@@ -1,4 +1,11 @@
 import type { Industry } from './game-state/types';
+import type { DefaultIndustryTypeName } from './default-industry-types';
+import type { DefaultCityName } from './default-cities';
 
-export const defaultIndustries: readonly Industry[] = [
+type DefaultIndustry = Omit<Industry, 'typeName' | 'city'> & {
+  typeName: DefaultIndustryTypeName;
+  city: DefaultCityName;
+};
+
+export const defaultIndustries: readonly DefaultIndustry[] = [
 ] as const;
